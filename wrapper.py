af7f170d19711a5cc1c8751aba8a85bc6b01e7fb
Max Bates <mbates5@luc.edu>
2025-02-21 13:21:15 -0600

    Add bowtie2

--- wrapper.py
+++ wrapper.py
@@ -9,23 +9,45 @@ def check_arg(args=None):
     parser.add_argument("-g", "--genome",
     help = "ref genome to map to",
     required = False)
+    parser.add_argument("-i1_1", "--input1_1",
+    help = "input genome 1 read 1 fastq",
+    required = False)
+    parser.add_argument("-i1_2", "--input1_2",
+    help = "input genome 1 read 2 fastq",
+    required = False)
+    parser.add_argument("-i2_1", "--input2_1",
+    help = "input genome 2 read 1 fastq",
+    required = False)
+    parser.add_argument("-i2_2", "--input2_2",
+    help = "input genome 2 read 2 fastq",
+    required = False)
     return parser.parse_args(args)
 
 #retrieve command line arguments
 arguments = check_arg(sys.argv[1:])
-'''infile = arguments.input
-outfile = arguments.output'''
-genome = arguments.genome
+genome_ref = arguments.genome
+in_genome_1_1 = arguments.input1_1
+in_genome_1_2 = arguments.input1_2
+in_genome_2_1 = arguments.input2_1
+in_genome_2_2 = arguments.input2_2
 
+home_dir = os.environ["HOME"]
+
+os.chdir(home_dir)
 os.system("mkdir -p PipelineProject_Max_Bates")
 os.chdir("./PipelineProject_Max_Bates")
-os.system("touch PipelineProject.log")
+
 # Ensuring log file is empty
 f = open("PipelineProject.log", 'w')
 f.close()
 
 ### TEST CODE FIX
-genome = "~/Github/Genomes/GCF_000845245.1_ViralProj14559_genomic.fna"
+genome_ref = "~/Github/Genomes/GCF_000845245.1_ViralProj14559_genomic.fna"
+in_genome_1_1 = "/home/2025/mbates5/Github/Genomes/SRR5660030_1.fastq"
+in_genome_1_2 = "/home/2025/mbates5/Github/Genomes/SRR5660030_2.fastq"
+in_genome_2_1 = "/home/2025/mbates5/Github/Genomes/SRR5660033_1.fastq"
+in_genome_2_2 = "/home/2025/mbates5/Github/Genomes/SRR5660033_2.fastq"
 
-os.system(f"bowtie2-build {genome} Genome_Index")
-
+os.system(f"bowtie2-build {genome_ref} Genome_Index")
+os.system(f"bowtie2 --quiet -x Genome_Index -1 {in_genome_1_1} -2 {in_genome_1_2} -S mapped1.sam -p 2 --al-conc-gz filtered1.fq.gz")
+os.system(f"bowtie2 --quiet -x Genome_Index -1 {in_genome_2_1} -2 {in_genome_2_2} -S mapped2.sam -p 2 --al-conc-gz filtered2.fq.gz")
