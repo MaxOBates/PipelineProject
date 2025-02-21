ee69dbaf1167750f43cda2af69d1e998aefd341c
Max Bates <mbates5@luc.edu>
2025-02-21 11:29:31 -0600

    Add bowtie2-build to create genome index.

--- wrapper.py
+++ wrapper.py
@@ -6,19 +6,26 @@ import argparse
 def check_arg(args=None):
     parser = argparse.ArgumentParser(
     description="ADD TITLE OF SCRIPT HERE (shows on help -h)")
-    parser.add_argument("-i", "--input",
-    help="input file",
-    required=True)
-    parser.add_argument("-o", "--output",
-    help="output file",
-    required=True)
+    parser.add_argument("-g", "--genome",
+    help = "ref genome to map to",
+    required = False)
     return parser.parse_args(args)
 
 #retrieve command line arguments
 arguments = check_arg(sys.argv[1:])
-infile = arguments.input
-outfile = arguments.output
+'''infile = arguments.input
+outfile = arguments.output'''
+genome = arguments.genome
 
 os.system("mkdir -p PipelineProject_Max_Bates")
 os.chdir("./PipelineProject_Max_Bates")
 os.system("touch PipelineProject.log")
+# Ensuring log file is empty
+f = open("PipelineProject.log", 'w')
+f.close()
+
+### TEST CODE FIX
+genome = "~/Github/Genomes/GCF_000845245.1_ViralProj14559_genomic.fna"
+
+os.system(f"bowtie2-build {genome} Genome_Index")
+
